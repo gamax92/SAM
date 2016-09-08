cd30780fe1b881fbbaa48b035e08d499078b9127
gamax92 <gamax92@aol.com>
2016-09-08 11:43:20 -0600

    Add in Makefile.js
    The Makefile.js is probably not the best
    But, it works.

---
+++ Makefile.js
@@ -0,0 +1,14 @@
+OBJS = reciter.o sam.o render.o nodebug.o
+
+CC = emcc
+
+CFLAGS = -Wall -O3 --memory-init-file 0 -s EXPORTED_FUNCTIONS="['_TextToPhonemes','_SetInput','_SAMMain','_GetBufferLength','_GetBuffer','_SetSpeed','_SetPitch','_SetMouth','_SetThroat','_EnableSingmode']"
+
+sam.js: $(OBJS)
+	$(CC) -o sam.js $(OBJS) $(CFLAGS)
+
+%.o: src/%.c
+	$(CC) $(CFLAGS) -c $<
+
+clean:
+	rm *.o
